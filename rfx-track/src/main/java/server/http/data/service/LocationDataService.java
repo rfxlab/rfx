6f6ba68241751b9d5e80b145bebc108f3118cd62
trieu <trieunt@fpt.com.vn>
2014-11-22 17:06:41 +0700

    update

---
+++ rfx-track/src/main/java/server/http/data/service/LocationDataService.java
@@ -0,0 +1,13 @@
+package server.http.data.service;
+
+
+
+public class LocationDataService {
+
+    
+   
+    //private static Map<String, Boolean> countryCacheMap = new HashMap<>(); 
+
+	//TODO
+
+}
