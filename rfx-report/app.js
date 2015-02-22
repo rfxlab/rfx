f8516b33a102c34d346be8c1e7bb869c0a1fad1d
trieu <trieunt@fpt.com.vn>
2015-02-22 16:13:27 +0700

    layout

--- rfx-report/app.js
+++ rfx-report/app.js
@@ -10,27 +10,10 @@ app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'admin.hbs'}));
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res){
-    res.render('index');
-});
+    var data = {numTreatments: 101};
+    data.dashboard_title = 'RFX Admin';
 
-app.get('/simple', function(req, res){
-    var data = {name: 'Gorilla'};
-    res.render('simple', data);
-});
-
-app.get('/complex', function(req, res){
-    var data = {
-        name: 'Gorilla',
-        address: {
-            streetName: 'Broadway',
-            streetNumber: '721',
-            floor: 4,
-            addressType: {
-                typeName: 'residential'
-            }
-        }
-    };
-    res.render('complex', data);
+    res.render('summary', data);
 });
 
 app.get('/loop', function(req, res){
